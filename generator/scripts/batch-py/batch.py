bd875845494a80c4afd012cb1894c61a07b52437
Philipp Obermeier <pobermei@gmail.com>
2017-10-15 18:02:22 +0200

    Change Python-based batch generation for templates
    - Add subcase selection for batch.py script
    - Add multi-stage incremental generation to batch.py
    - Add template suffix to instance dir in batch.py
    - Add relevance for order of subcats in batch.py

--- generator/scripts/batch-py/batch.py
+++ generator/scripts/batch-py/batch.py
@@ -8,19 +8,25 @@ import shlex
 import argparse
 from collections import OrderedDict
 from glob import glob
-from cases import define_cases, general_pars
+from cases_workaround import define_cases, general_pars
 
 def gen_instances(cases, args):
     if args.cats:
-        cases_items = {cat : cases[cat] for cat in args.cats}.items()
-    else:
-        cases_items = cases.items()
+        cases = {cat : cases[cat] for cat in args.cats}
+    if args.subcats:
+        for cat in cases:
+            # cases[cat] = {_cat : cases[cat][_cat] for _cat in args.subcats}
+            filtered_cat = OrderedDict()
+            for _cat in args.subcats:
+                filtered_cat[_cat] = cases[cat][_cat]
+            cases[cat] = filtered_cat
     if args.split:
         split = '--split {0} {1}'.format(args.split[0], args.split[1])
     else:
         split = ''
-    for cat, _cases in cases_items:
+    for cat, _cases in cases.items():
         for subcat, subcases in _cases.items():
+            template_dir = template_suffix = None
             for case in subcases:
                 case_dir = subcat
                 case_pars = case['pars']
@@ -40,29 +46,56 @@ def gen_instances(cases, args):
                     {case_pars}'''.format(threads=args.threads,
                                           wait=args.wait,
                                           case_pars=case_pars))
-
                 if 'template' in case: # Generate templates
-                    if args.split:
-                        models = args.split[0]
-                    else:
-                        models = args.models
+                    # if args.split:
+                    #     models = args.split[0]
+                    # else:
+                    #     models = args.models
                     dest += '/templates'
-                    command = list(command_common)
-                    command.extend(shlex.split(
-                        '''
-                        -N {models}
-                        -d {dest}
-                        '''.format(models=models, dest=dest)))
-                    print '*** Running TEMPLATE Command:'
-                    run_command(command, dest, True)
-                else: # Generate instances either
-                    templates = sorted(glob(r'{dest_pre}/templates/*.lp'.format(dest_pre=dest_pre)))
-                    if templates: # ... with a template; or
+                    if 'suffix' in case:
+                        dest += '/' + case['suffix']
+                        template_suffix = case['suffix']
+                    else:
+                        template_suffix = ''
+                    # Add template option if template uses in turn another template
+                    if  template_dir:
+                        templates = sorted(glob(r'{template_dir}/*.lp'.format(
+                            template_dir=template_dir)))
+                        for idx, temp in enumerate(templates):
+                            command = list(command_common)
+                            command.extend(shlex.split(
+                                '''
+                                -f PrevTemp{idx}_
+                                -N 1
+                                -T {template}
+                                -d {dest}
+                                '''.format(idx=idx, models=1, template=temp, dest=dest)))
+                            print '*** Running NESTED TEMPLATE Command:'
+                            run_command(command, dest, False)
+                    else:
+                        command = list(command_common)
+                        command.extend(shlex.split(
+                            '''
+                            -N {models}
+                            -d {dest}
+                            '''.format(models=args.templates, dest=dest)))
+                        print '*** Running TEMPLATE Command:'
+                        run_command(command, dest, True)
+                    template_dir = dest
+                # Otherwise, generate instances either
+                else:
+                    if template_dir: # ... with a template; or
+                        templates = sorted(glob(r'{template_dir}/*.lp'.format(
+                            template_dir=template_dir)))
                         if args.split:
                             split = '--split 1 {}'.format(args.split[1])
                         for idx, temp in enumerate(templates):
-                            dest = '{dest_pre}/instances_T{idx}'.format(dest_pre=dest_pre,
-                                                                        idx=str(idx+1))
+                            dest = '{dest_pre}/instances/per_template/{temp_suffix}_T{idx}'.format(
+                                dest_pre=dest_pre,
+                                temp_suffix=template_suffix,
+                                idx=str(idx+1))
+                            if 'suffix' in case:
+                                dest += '/' + case['suffix']
                             command = list(command_common)
                             command.extend(shlex.split(
                                 '''
@@ -76,8 +109,11 @@ def gen_instances(cases, args):
                                            dest=dest)))
                             print '*** Running COMBINED Command:'
                             run_command(command, dest)
+                        template_dir = template_suffix = None
                     else: # ... regular, i.e., w/o template
                         command = list(command_common)
+                        if 'suffix' in case:
+                            dest += '/' + case['suffix']
                         command.extend(shlex.split(
                             '''
                             -N {models}
@@ -100,12 +136,14 @@ def main():
     parser = argparse.ArgumentParser(
         usage='Extracts facts from json file read from stdin or give as argument')
     parser.add_argument('-c', '--cats', nargs='*', help='optional list of benchmark categories to create')
+    parser.add_argument('-s', '--subcats', nargs='*', help='optional list of benchmark sub-categories to create')
     parser.add_argument('-d', '--directory', type=str, default='generatedInstances',
                         help="the directory to safe the files")
     parser.add_argument('-t', '--threads', type=int, help='number of clasp threads', default=1)
     parser.add_argument('-w' , '--wait', type=int,
                         help='time to wait in seconds before solving is aborted if not finished yet', default=300)
     parser.add_argument('-N', '--models', type=int, help='number of instances', default=10)
+    parser.add_argument('--TN', '--templates', dest='templates', type=int, help='number of template instances', default=1)
     parser.add_argument('--split', nargs=2, type=int,
                         help='''splits instances into warehouse and
                                   order-related facts; takes as arguments 1.)
